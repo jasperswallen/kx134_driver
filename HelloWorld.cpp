b31f5753c75e3960f5bd89d27e5fe376e9b9ff9a
Jasper Swallen <swallen@usc.edu>
2020-08-17 17:46:24 -0400

    initial commit, forking from mbed-cmake-example

---
+++ HelloWorld.cpp
@@ -0,0 +1,13 @@
+#include "mbed.h"
+
+BufferedSerial serial(USBTX, USBRX, 115200);
+
+int main() {
+
+	while(true)
+	{
+		char const message[] = "Hello world!\r\n";
+		serial.write(message, sizeof(message));
+		ThisThread::sleep_for(1000);
+	}
+}
