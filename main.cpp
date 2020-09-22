02055721c4d7973ae668fda0c6af9a0ba8eef863
Jasper Swallen <swallen@usc.edu>
2020-09-22 17:17:18 -0400

    get standard deviations

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "KX134.h"
+#include "math.h"
 #include "mbed.h"
 
 #include <inttypes.h>
@@ -24,13 +25,22 @@ int main(void)
 
     printf("Successfully initialized KX134\r\n");
 
+    int hz = 6400;
+
     // initialize asynch reading
     kx134Obj.enableRegisterWriting();
-    kx134Obj.setOutputDataRate(50);
-    kx134Obj.setAccelRange(32);
+    kx134Obj.setOutputDataRateHz(hz);
+    kx134Obj.setAccelRange(KX134::Range::RANGE_64G);
+    kx134Obj.disableRegisterWriting();
 
-    while(1)
+    int numTrials = 200;
+    float gravs[numTrials][3];
+
+    for(size_t trialIndex = 0; trialIndex < numTrials; ++trialIndex)
     {
+        float time = 1.0 / (float)hz * 1000.0;
+        ThisThread::sleep_for(time);
+
         int16_t output[3];
         kx134Obj.getAccelerations(output);
         float ax = kx134Obj.convertRawToGravs(output[0]);
@@ -42,6 +52,49 @@ int main(void)
                output[0], output[1], output[2]);
         printf("KX134 Accel in Gravs: X: %f g, Y: %f g, Z: %f g \r\n", ax, ay,
                az);
-        ThisThread::sleep_for(1000ms);
+
+        gravs[trialIndex][0] = ax;
+        gravs[trialIndex][1] = ay;
+        gravs[trialIndex][2] = az;
     }
+
+    float average[3] = {0};
+    float maxGravs[3] = {0};
+    float minGravs[3] = {std::numeric_limits<float>::max()};
+
+    for(size_t trialIndex = 0; trialIndex < numTrials; ++trialIndex)
+    {
+        for(int i = 0; i < 3; ++i)
+        {
+            average[i] += gravs[trialIndex][i];
+            maxGravs[i] = std::max(gravs[trialIndex][i], maxGravs[i]);
+            minGravs[i] = std::min(gravs[trialIndex][i], minGravs[i]);
+        }
+    }
+
+    for(int i = 0; i < 3; ++i)
+    {
+        average[i] /= numTrials;
+    }
+
+    float variance[3] = {0};
+    for(size_t trialIndex = 0; trialIndex < numTrials; ++trialIndex)
+    {
+        for(int i = 0; i < 3; ++i)
+        {
+            variance[i] += pow(gravs[trialIndex][i] - average[i], 2);
+        }
+    }
+
+    float stdDeviation[3];
+    for(int i = 0; i < 3; ++i)
+    {
+        variance[i] /= numTrials - 1;
+        stdDeviation[i] = sqrt(variance[i]);
+    }
+
+    printf("Average Gravs at %d Hz: %f x, %f y, %f z\n", hz, average[0],
+           average[1], average[2]);
+    printf("Standard Deviation at %d Hz: %f x, %f y, %f z\n", hz,
+           stdDeviation[0], stdDeviation[1], stdDeviation[2]);
 }
