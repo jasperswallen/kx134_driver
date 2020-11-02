270ee48544d87873dc93f0c05c917b444b86b531
Jasper Swallen <swallen@usc.edu>
2020-11-02 16:23:46 -0500

    testing

---
+++ KX134/SerialStream.h
@@ -0,0 +1,55 @@
+#ifndef SERIALSTREAM_H
+#define SERIALSTREAM_H
+
+#include <mbed.h>
+#include <platform/Stream.h>
+
+/**
+ * SerialStream
+ * Bringing MBed serial ports back like it's 1999... or at least 2019.
+ *
+ * This class adapts an MBed 6.0 serial port class into a Stream instance.
+ * This lets you do two useful things with it:
+ * - Call printf() and scanf() on it
+ * - Pass it to code that expects a Stream to print things on.
+ *
+ */
+template <class SerialClass> class SerialStream : public Stream
+{
+    SerialClass &serialClass;
+
+  public:
+    /**
+     * Create a SerialStream from a serial port.
+     * @param _serialClass BufferedSerial or UnbufferedSerial instance
+     * @param name The name of the stream associated with this serial port
+     * (optional)
+     */
+    SerialStream(SerialClass &_serialClass, const char *name = nullptr)
+        : Stream(name), serialClass(_serialClass)
+    {
+    }
+
+  private:
+    // override Stream::read() and write() to call serial class directly.
+    // This avoids the overhead of feeding in individual characters.
+    virtual ssize_t write(const void *buffer, size_t length)
+    {
+        return serialClass.write(buffer, length);
+    }
+
+    virtual ssize_t read(void *buffer, size_t length)
+    {
+        return serialClass.read(buffer, length);
+    }
+
+    // Dummy implementations -- these will never be called because we override
+    // write() and read() instead. but we have to override them since they're
+    // pure virtual.
+    virtual int _putc(int c) { return 0; }
+    virtual int _getc() { return 0; }
+
+    template <typename F> void attach(F &&func) { serialClass.attach(func); }
+};
+
+#endif // SERIALSTREAM_H
--- main.cpp
+++ main.cpp
@@ -12,89 +12,122 @@
 #define PIN_INT2 PA_0 // placeholder
 #define PIN_RST PA_0  // placeholder
 
-int main(void)
+// int main(void)
+// {
+//     KX134 kx134Obj(PIN_SPI_MOSI, PIN_SPI_MISO, PIN_SPI_SCK, PIN_SPI_CS,
+//                    PIN_INT1, PIN_INT2, PIN_RST);
+
+//     if(!kx134Obj.init())
+//     {
+//         printf("Failed to initialize KX134\r\n");
+//         return 1;
+//     }
+
+//     printf("Successfully initialized KX134\r\n");
+
+//     int hz = 25600;
+
+//     // initialize asynch reading
+//     kx134Obj.enableRegisterWriting();
+//     kx134Obj.setOutputDataRateHz(hz);
+//     kx134Obj.setAccelRange(KX134::Range::RANGE_64G);
+//     kx134Obj.disableRegisterWriting();
+
+//     int numTrials = 200;
+//     float gravs[numTrials][3];
+
+//     for(size_t trialIndex = 0; trialIndex < numTrials; ++trialIndex)
+//     {
+//         while(!kx134Obj.dataReady())
+//             ;
+
+//         int16_t output[3];
+//         kx134Obj.getAccelerations(output);
+//         float ax = kx134Obj.convertRawToGravs(output[0]);
+//         float ay = kx134Obj.convertRawToGravs(output[1]);
+//         float az = kx134Obj.convertRawToGravs(output[2]);
+
+//         printf("KX134 Accel: X: %" PRIi16 " LSB, Y: %" PRIi16
+//                " LSB, Z: %" PRIi16 " LSB \r\n",
+//                output[0], output[1], output[2]);
+//         printf("KX134 Accel in Gravs: X: %f g, Y: %f g, Z: %f g \r\n", ax,
+//         ay,
+//                az);
+
+//         gravs[trialIndex][0] = ax;
+//         gravs[trialIndex][1] = ay;
+//         gravs[trialIndex][2] = az;
+//     }
+
+//     float average[3] = {0};
+//     float maxGravs[3] = {0};
+//     float minGravs[3] = {std::numeric_limits<float>::max()};
+
+//     for(size_t trialIndex = 0; trialIndex < numTrials; ++trialIndex)
+//     {
+//         for(int i = 0; i < 3; ++i)
+//         {
+//             average[i] += gravs[trialIndex][i];
+//             maxGravs[i] = std::max(gravs[trialIndex][i], maxGravs[i]);
+//             minGravs[i] = std::min(gravs[trialIndex][i], minGravs[i]);
+//         }
+//     }
+
+//     for(int i = 0; i < 3; ++i)
+//     {
+//         average[i] /= numTrials;
+//     }
+
+//     float variance[3] = {0};
+//     for(size_t trialIndex = 0; trialIndex < numTrials; ++trialIndex)
+//     {
+//         for(int i = 0; i < 3; ++i)
+//         {
+//             variance[i] += pow(gravs[trialIndex][i] - average[i], 2);
+//         }
+//     }
+
+//     float stdDeviation[3];
+//     for(int i = 0; i < 3; ++i)
+//     {
+//         variance[i] /= numTrials - 1;
+//         stdDeviation[i] = sqrt(variance[i]);
+//     }
+
+//     printf("Average Gravs at %d Hz: %f x, %f y, %f z\n", hz, average[0],
+//            average[1], average[2]);
+//     printf("Standard Deviation at %d Hz: %f x, %f y, %f z\n", hz,
+//            stdDeviation[0], stdDeviation[1], stdDeviation[2]);
+// }
+
+#include "SerialStream.h"
+
+#define BAUDRATE 115200
+
+BufferedSerial serial(USBTX, USBRX, BAUDRATE);
+SerialStream<BufferedSerial> pc(serial);
+
+void rxCallback()
 {
-    KX134 kx134Obj(PIN_SPI_MOSI, PIN_SPI_MISO, PIN_SPI_SCK, PIN_SPI_CS,
-                   PIN_INT1, PIN_INT2, PIN_RST);
+    pc.printf("?");
+}
 
-    if(!kx134Obj.init())
+int main()
+{
+    pc.printf("hi\r\n");
+    uint8_t buf[10];
+    int size = pc.scanf("%9s", buf);
+    pc.printf("%u %s\r\n", size, buf);
+
+    int event = SERIAL_EVENT_RX_COMPLETE | SERIAL_EVENT_RX_OVERFLOW;
+
+    serial.read(buf, 10, callback(&rxCallback), event, '\n');
+
+    int i = 0;
+    while(1)
     {
-        printf("Failed to initialize KX134\r\n");
-        return 1;
+        pc.printf("%i\t", i);
+        ThisThread::sleep_for(1s);
+        i++;
     }
-
-    printf("Successfully initialized KX134\r\n");
-
-    int hz = 25600;
-
-    // initialize asynch reading
-    kx134Obj.enableRegisterWriting();
-    kx134Obj.setOutputDataRateHz(hz);
-    kx134Obj.setAccelRange(KX134::Range::RANGE_64G);
-    kx134Obj.disableRegisterWriting();
-
-    int numTrials = 200;
-    float gravs[numTrials][3];
-
-    for(size_t trialIndex = 0; trialIndex < numTrials; ++trialIndex)
-    {
-        while(!kx134Obj.dataReady())
-            ;
-
-        int16_t output[3];
-        kx134Obj.getAccelerations(output);
-        float ax = kx134Obj.convertRawToGravs(output[0]);
-        float ay = kx134Obj.convertRawToGravs(output[1]);
-        float az = kx134Obj.convertRawToGravs(output[2]);
-
-        printf("KX134 Accel: X: %" PRIi16 " LSB, Y: %" PRIi16
-               " LSB, Z: %" PRIi16 " LSB \r\n",
-               output[0], output[1], output[2]);
-        printf("KX134 Accel in Gravs: X: %f g, Y: %f g, Z: %f g \r\n", ax, ay,
-               az);
-
-        gravs[trialIndex][0] = ax;
-        gravs[trialIndex][1] = ay;
-        gravs[trialIndex][2] = az;
-    }
-
-    float average[3] = {0};
-    float maxGravs[3] = {0};
-    float minGravs[3] = {std::numeric_limits<float>::max()};
-
-    for(size_t trialIndex = 0; trialIndex < numTrials; ++trialIndex)
-    {
-        for(int i = 0; i < 3; ++i)
-        {
-            average[i] += gravs[trialIndex][i];
-            maxGravs[i] = std::max(gravs[trialIndex][i], maxGravs[i]);
-            minGravs[i] = std::min(gravs[trialIndex][i], minGravs[i]);
-        }
-    }
-
-    for(int i = 0; i < 3; ++i)
-    {
-        average[i] /= numTrials;
-    }
-
-    float variance[3] = {0};
-    for(size_t trialIndex = 0; trialIndex < numTrials; ++trialIndex)
-    {
-        for(int i = 0; i < 3; ++i)
-        {
-            variance[i] += pow(gravs[trialIndex][i] - average[i], 2);
-        }
-    }
-
-    float stdDeviation[3];
-    for(int i = 0; i < 3; ++i)
-    {
-        variance[i] /= numTrials - 1;
-        stdDeviation[i] = sqrt(variance[i]);
-    }
-
-    printf("Average Gravs at %d Hz: %f x, %f y, %f z\n", hz, average[0],
-           average[1], average[2]);
-    printf("Standard Deviation at %d Hz: %f x, %f y, %f z\n", hz,
-           stdDeviation[0], stdDeviation[1], stdDeviation[2]);
 }
