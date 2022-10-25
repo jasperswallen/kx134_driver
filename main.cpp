ce43055aab195a4236047184101738371bb22dc3
Jasper Swallen <swallen@usc.edu>
2022-10-25 00:51:34 -0700

    add extern C

--- main.cpp
+++ main.cpp
@@ -25,6 +25,9 @@
 
 #include "mbed.h"
 
+extern "C"
+{
+
 /** @addtogroup STM32H7xx_HAL_Examples
   * @{
   */
@@ -60,10 +63,8 @@ ALIGN_32BYTES(uint8_t aRxBuffer[BUFFER_ALIGNED_SIZE]);
 __IO uint32_t wTransferState = TRANSFER_WAIT;
 
 /* Private function prototypes -----------------------------------------------*/
-static void SystemClock_Config(void);
 static void Error_Handler(void);
 static uint16_t Buffercmp(uint8_t *pBuffer1, uint8_t *pBuffer2, uint16_t BufferLength);
-static void CPU_CACHE_Enable(void);
 
 /* Private functions ---------------------------------------------------------*/
 
@@ -78,24 +79,7 @@ DigitalOut led_3(LED3, 0);
   */
 int main(void)
 {
-  /* Enable the CPU Cache */
-  CPU_CACHE_Enable();
-
-  /* STM32H7xx HAL library initialization:
-       - Systick timer is configured by default as source of time base, but user 
-         can eventually implement his proper time base source (a general purpose 
-         timer for example or other time source), keeping in mind that Time base 
-         duration should be kept 1ms since PPP_TIMEOUT_VALUEs are defined and 
-         handled in milliseconds basis.
-       - Set NVIC Group Priority to 4
-       - Low Level Initialization
-     */
-  HAL_Init();
-
-  /* Configure the system clock to 400 MHz */
-//   SystemClock_Config();
-
-  /* Configure LED1, LED2 and LED3 */
+  printf("\r\nStarting main...\r\n");
 
   /*##-1- Configure the SPI peripheral #######################################*/
   /* Set the SPI parameters */
@@ -128,17 +112,19 @@ int main(void)
 
 #ifdef MASTER_BOARD
   /* Configure User push-button button */
-//   BSP_PB_Init(BUTTON_USER,BUTTON_MODE_GPIO);
   DigitalIn user_button(BUTTON1);
+  printf("Waiting for button press");
   /* Wait for User push-button press before starting the Communication */
   while (!user_button)
   {
     // BSP_LED_Toggle(LED1);
     led_1 = !led_1;
     HAL_Delay(100);
+    printf(".");
+    fflush(stdout);
   }
-//   BSP_LED_Off(LED1);
   led_1 = false;
+  printf("\r\nGot button press\r\n");
 #endif /* MASTER_BOARD */
 
   /*##-2- Start the Full Duplex Communication process ########################*/  
@@ -160,6 +146,8 @@ int main(void)
   {
   }
   
+    printf("Transaction finished! %lu\r\n", wTransferState);
+
   /* Invalidate cache prior to access by CPU */
   SCB_InvalidateDCache_by_Addr ((uint32_t *)aRxBuffer, BUFFERSIZE);
 
@@ -184,85 +172,6 @@ int main(void)
   }
 }
 
-/**
-  * @brief  System Clock Configuration
-  *         The system Clock is configured as follow : 
-  *            System Clock source            = PLL (HSE BYPASS)
-  *            SYSCLK(Hz)                     = 400000000 (CPU Clock)
-  *            HCLK(Hz)                       = 200000000 (AXI and AHBs Clock)
-  *            AHB Prescaler                  = 2
-  *            D1 APB3 Prescaler              = 2 (APB3 Clock  100MHz)
-  *            D2 APB1 Prescaler              = 2 (APB1 Clock  100MHz)
-  *            D2 APB2 Prescaler              = 2 (APB2 Clock  100MHz)
-  *            D3 APB4 Prescaler              = 2 (APB4 Clock  100MHz)
-  *            HSE Frequency(Hz)              = 8000000
-  *            PLL_M                          = 4
-  *            PLL_N                          = 400
-  *            PLL_P                          = 2
-  *            PLL_Q                          = 4
-  *            PLL_R                          = 2
-  *            VDD(V)                         = 3.3
-  *            Flash Latency(WS)              = 4
-  * @param  None
-  * @retval None
-  */
-static void SystemClock_Config(void)
-{
-  RCC_ClkInitTypeDef RCC_ClkInitStruct;
-  RCC_OscInitTypeDef RCC_OscInitStruct;
-  HAL_StatusTypeDef ret = HAL_OK;
-  
-  /*!< Supply configuration update enable */
-  HAL_PWREx_ConfigSupply(PWR_LDO_SUPPLY);
-
-  /* The voltage scaling allows optimizing the power consumption when the device is
-     clocked below the maximum system frequency, to update the voltage scaling value
-     regarding system frequency refer to product datasheet.  */
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
-
-  while(!__HAL_PWR_GET_FLAG(PWR_FLAG_VOSRDY)) {}
-  
-  /* Enable HSE Oscillator and activate PLL with HSE as source */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
-  RCC_OscInitStruct.HSIState = RCC_HSI_OFF;
-  RCC_OscInitStruct.CSIState = RCC_CSI_OFF;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-
-  RCC_OscInitStruct.PLL.PLLM = 4;
-  RCC_OscInitStruct.PLL.PLLN = 400;
-  RCC_OscInitStruct.PLL.PLLFRACN = 0;
-  RCC_OscInitStruct.PLL.PLLP = 2;
-  RCC_OscInitStruct.PLL.PLLR = 2;
-  RCC_OscInitStruct.PLL.PLLQ = 4;
-
-  RCC_OscInitStruct.PLL.PLLVCOSEL = RCC_PLL1VCOWIDE;
-  RCC_OscInitStruct.PLL.PLLRGE = RCC_PLL1VCIRANGE_1;
-  ret = HAL_RCC_OscConfig(&RCC_OscInitStruct);
-  if(ret != HAL_OK)
-  {
-    Error_Handler();
-  }
-  
-/* Select PLL as system clock source and configure  bus clocks dividers */
-  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_D1PCLK1 | RCC_CLOCKTYPE_PCLK1 | \
-                                 RCC_CLOCKTYPE_PCLK2  | RCC_CLOCKTYPE_D3PCLK1);
-
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.SYSCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB3CLKDivider = RCC_APB3_DIV2;  
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_APB1_DIV2; 
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_APB2_DIV2; 
-  RCC_ClkInitStruct.APB4CLKDivider = RCC_APB4_DIV2; 
-  ret = HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4);
-  if(ret != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-}
 /**
   * @brief  TxRx Transfer completed callback.
   * @param  hspi: SPI handle
@@ -353,20 +262,6 @@ void assert_failed(uint8_t* file, uint32_t line)
 #endif
 
 /**
-  * @brief  CPU L1-Cache enable.
-  * @param  None
-  * @retval None
-  */
-static void CPU_CACHE_Enable(void)
-{
-  /* Enable I-Cache */
-  SCB_EnableICache();
-
-  /* Enable D-Cache */
-  SCB_EnableDCache();
-}
-
-/**
   * @}
   */
 
@@ -377,82 +272,6 @@ static void CPU_CACHE_Enable(void)
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
 
 
-
-
-
-
-
-
-
-/**
-  * @brief   This function handles NMI exception.
-  * @param  None
-  * @retval None
-  */
-void NMI_Handler(void)
-{
-}
-
-/**
-  * @brief  This function handles Hard Fault exception.
-  * @param  None
-  * @retval None
-  */
-void HardFault_Handler(void)
-{
-  /* Go to infinite loop when Hard Fault exception occurs */
-  while (1)
-  {
-  }
-}
-
-/**
-  * @brief  This function handles Memory Manage exception.
-  * @param  None
-  * @retval None
-  */
-void MemManage_Handler(void)
-{
-  /* Go to infinite loop when Memory Manage exception occurs */
-  while (1)
-  {
-  }
-}
-
-/**
-  * @brief  This function handles Bus Fault exception.
-  * @param  None
-  * @retval None
-  */
-void BusFault_Handler(void)
-{
-  /* Go to infinite loop when Bus Fault exception occurs */
-  while (1)
-  {
-  }
-}
-
-/**
-  * @brief  This function handles Usage Fault exception.
-  * @param  None
-  * @retval None
-  */
-void UsageFault_Handler(void)
-{
-  /* Go to infinite loop when Usage Fault exception occurs */
-  while (1)
-  {
-  }
-}
-
-/**
-  * @brief  This function handles Debug Monitor exception.
-  * @param  None
-  * @retval None
-  */
-void DebugMon_Handler(void)
-{
-}
 
 /******************************************************************************/
 /*                 STM32H7xx Peripherals Interrupt Handlers                   */
@@ -498,3 +317,5 @@ void SPIx_IRQHandler(void)
 /*void PPP_IRQHandler(void)
 {
 }*/
+
+};
---
+++ mbed-cmake-config/config-headers/NUCLEO_H743ZI2/mbed_config.h
@@ -0,0 +1,127 @@
+/*
+ * mbed SDK
+ * Copyright (c) 2017 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Automatically generated configuration file.
+// DO NOT EDIT, content will be overwritten.
+
+#ifndef __MBED_CONFIG_DATA__
+#define __MBED_CONFIG_DATA__
+
+// Configuration parameters
+#define CLOCK_SOURCE                                                      USE_PLL_HSE_EXTC|USE_PLL_HSI                                 // set by target:MCU_STM32H7
+#define HSE_VALUE                                                         8000000                                                      // set by target:NUCLEO_H743ZI2
+#define LPTICKER_DELAY_TICKS                                              0                                                            // set by target:MCU_STM32H7
+#define MBED_CONF_DRIVERS_OSPI_CSN                                        OSPI_FLASH1_CSN                                              // set by library:drivers
+#define MBED_CONF_DRIVERS_OSPI_DQS                                        OSPI_FLASH1_DQS                                              // set by library:drivers
+#define MBED_CONF_DRIVERS_OSPI_IO0                                        OSPI_FLASH1_IO0                                              // set by library:drivers
+#define MBED_CONF_DRIVERS_OSPI_IO1                                        OSPI_FLASH1_IO1                                              // set by library:drivers
+#define MBED_CONF_DRIVERS_OSPI_IO2                                        OSPI_FLASH1_IO2                                              // set by library:drivers
+#define MBED_CONF_DRIVERS_OSPI_IO3                                        OSPI_FLASH1_IO3                                              // set by library:drivers
+#define MBED_CONF_DRIVERS_OSPI_IO4                                        OSPI_FLASH1_IO4                                              // set by library:drivers
+#define MBED_CONF_DRIVERS_OSPI_IO5                                        OSPI_FLASH1_IO5                                              // set by library:drivers
+#define MBED_CONF_DRIVERS_OSPI_IO6                                        OSPI_FLASH1_IO6                                              // set by library:drivers
+#define MBED_CONF_DRIVERS_OSPI_IO7                                        OSPI_FLASH1_IO7                                              // set by library:drivers
+#define MBED_CONF_DRIVERS_OSPI_SCK                                        OSPI_FLASH1_SCK                                              // set by library:drivers
+#define MBED_CONF_DRIVERS_QSPI_CSN                                        QSPI_FLASH1_CSN                                              // set by library:drivers
+#define MBED_CONF_DRIVERS_QSPI_IO0                                        QSPI_FLASH1_IO0                                              // set by library:drivers
+#define MBED_CONF_DRIVERS_QSPI_IO1                                        QSPI_FLASH1_IO1                                              // set by library:drivers
+#define MBED_CONF_DRIVERS_QSPI_IO2                                        QSPI_FLASH1_IO2                                              // set by library:drivers
+#define MBED_CONF_DRIVERS_QSPI_IO3                                        QSPI_FLASH1_IO3                                              // set by library:drivers
+#define MBED_CONF_DRIVERS_QSPI_SCK                                        QSPI_FLASH1_SCK                                              // set by library:drivers
+#define MBED_CONF_DRIVERS_UART_SERIAL_RXBUF_SIZE                          256                                                          // set by library:drivers
+#define MBED_CONF_DRIVERS_UART_SERIAL_TXBUF_SIZE                          256                                                          // set by library:drivers
+#define MBED_CONF_EVENTS_PRESENT                                          1                                                            // set by library:events
+#define MBED_CONF_EVENTS_SHARED_DISPATCH_FROM_APPLICATION                 0                                                            // set by library:events
+#define MBED_CONF_EVENTS_SHARED_EVENTSIZE                                 768                                                          // set by library:events
+#define MBED_CONF_EVENTS_SHARED_HIGHPRIO_EVENTSIZE                        256                                                          // set by library:events
+#define MBED_CONF_EVENTS_SHARED_HIGHPRIO_STACKSIZE                        1024                                                         // set by library:events
+#define MBED_CONF_EVENTS_SHARED_STACKSIZE                                 2048                                                         // set by library:events
+#define MBED_CONF_EVENTS_USE_LOWPOWER_TIMER_TICKER                        0                                                            // set by library:events
+#define MBED_CONF_FLASHIAP_BLOCK_DEVICE_BASE_ADDRESS                      0xFFFFFFFF                                                   // set by library:flashiap-block-device
+#define MBED_CONF_FLASHIAP_BLOCK_DEVICE_SIZE                              0                                                            // set by library:flashiap-block-device
+#define MBED_CONF_MBED_TRACE_ENABLE                                       1                                                            // set by application[*]
+#define MBED_CONF_PLATFORM_CALLBACK_COMPARABLE                            1                                                            // set by library:platform
+#define MBED_CONF_PLATFORM_CALLBACK_NONTRIVIAL                            0                                                            // set by library:platform
+#define MBED_CONF_PLATFORM_CRASH_CAPTURE_ENABLED                          0                                                            // set by library:platform
+#define MBED_CONF_PLATFORM_CTHUNK_COUNT_MAX                               8                                                            // set by library:platform
+#define MBED_CONF_PLATFORM_DEFAULT_SERIAL_BAUD_RATE                       9600                                                         // set by library:platform
+#define MBED_CONF_PLATFORM_ERROR_ALL_THREADS_INFO                         0                                                            // set by library:platform
+#define MBED_CONF_PLATFORM_ERROR_FILENAME_CAPTURE_ENABLED                 0                                                            // set by library:platform
+#define MBED_CONF_PLATFORM_ERROR_HIST_ENABLED                             0                                                            // set by library:platform
+#define MBED_CONF_PLATFORM_ERROR_HIST_SIZE                                4                                                            // set by library:platform
+#define MBED_CONF_PLATFORM_ERROR_REBOOT_MAX                               1                                                            // set by library:platform
+#define MBED_CONF_PLATFORM_FATAL_ERROR_AUTO_REBOOT_ENABLED                0                                                            // set by library:platform
+#define MBED_CONF_PLATFORM_MAX_ERROR_FILENAME_LEN                         16                                                           // set by library:platform
+#define MBED_CONF_PLATFORM_MINIMAL_PRINTF_ENABLE_64_BIT                   1                                                            // set by library:platform
+#define MBED_CONF_PLATFORM_MINIMAL_PRINTF_ENABLE_FLOATING_POINT           0                                                            // set by library:platform
+#define MBED_CONF_PLATFORM_MINIMAL_PRINTF_SET_FLOATING_POINT_MAX_DECIMALS 6                                                            // set by library:platform
+#define MBED_CONF_PLATFORM_POLL_USE_LOWPOWER_TIMER                        0                                                            // set by library:platform
+#define MBED_CONF_PLATFORM_STDIO_BAUD_RATE                                115200                                                       // set by application[*]
+#define MBED_CONF_PLATFORM_STDIO_BUFFERED_SERIAL                          1                                                            // set by application[*]
+#define MBED_CONF_PLATFORM_STDIO_CONVERT_NEWLINES                         1                                                            // set by library:platform
+#define MBED_CONF_PLATFORM_STDIO_CONVERT_TTY_NEWLINES                     1                                                            // set by library:platform
+#define MBED_CONF_PLATFORM_STDIO_FLUSH_AT_EXIT                            1                                                            // set by library:platform
+#define MBED_CONF_PLATFORM_STDIO_MINIMAL_CONSOLE_ONLY                     0                                                            // set by library:platform
+#define MBED_CONF_PLATFORM_USE_MPU                                        1                                                            // set by library:platform
+#define MBED_CONF_RTOS_API_PRESENT                                        1                                                            // set by library:rtos-api
+#define MBED_CONF_RTOS_ENABLE_ALL_RTX_EVENTS                              0                                                            // set by library:rtos
+#define MBED_CONF_RTOS_EVFLAGS_NUM                                        0                                                            // set by library:rtos
+#define MBED_CONF_RTOS_IDLE_THREAD_STACK_SIZE                             512                                                          // set by library:rtos
+#define MBED_CONF_RTOS_IDLE_THREAD_STACK_SIZE_DEBUG_EXTRA                 128                                                          // set by library:rtos[STM]
+#define MBED_CONF_RTOS_IDLE_THREAD_STACK_SIZE_TICKLESS_EXTRA              256                                                          // set by library:rtos
+#define MBED_CONF_RTOS_MAIN_THREAD_STACK_SIZE                             4096                                                         // set by library:rtos
+#define MBED_CONF_RTOS_MSGQUEUE_DATA_SIZE                                 0                                                            // set by library:rtos
+#define MBED_CONF_RTOS_MSGQUEUE_NUM                                       0                                                            // set by library:rtos
+#define MBED_CONF_RTOS_MUTEX_NUM                                          0                                                            // set by library:rtos
+#define MBED_CONF_RTOS_PRESENT                                            1                                                            // set by library:rtos
+#define MBED_CONF_RTOS_SEMAPHORE_NUM                                      0                                                            // set by library:rtos
+#define MBED_CONF_RTOS_THREAD_NUM                                         0                                                            // set by library:rtos
+#define MBED_CONF_RTOS_THREAD_STACK_SIZE                                  4096                                                         // set by library:rtos
+#define MBED_CONF_RTOS_THREAD_USER_STACK_SIZE                             0                                                            // set by library:rtos
+#define MBED_CONF_RTOS_TIMER_NUM                                          0                                                            // set by library:rtos
+#define MBED_CONF_RTOS_TIMER_THREAD_STACK_SIZE                            768                                                          // set by library:rtos
+#define MBED_CONF_TARGET_BOOT_STACK_SIZE                                  0x400                                                        // set by library:rtos[*]
+#define MBED_CONF_TARGET_CONSOLE_UART                                     1                                                            // set by target:Target
+#define MBED_CONF_TARGET_CUSTOM_TICKERS                                   1                                                            // set by target:Target
+#define MBED_CONF_TARGET_DEEP_SLEEP_LATENCY                               4                                                            // set by target:MCU_STM32
+#define MBED_CONF_TARGET_DEFAULT_ADC_VREF                                 NAN                                                          // set by target:Target
+#define MBED_CONF_TARGET_GPIO_RESET_AT_INIT                               0                                                            // set by target:MCU_STM32
+#define MBED_CONF_TARGET_I2C_TIMING_VALUE_ALGO                            0                                                            // set by target:MCU_STM32H7
+#define MBED_CONF_TARGET_INIT_US_TICKER_AT_BOOT                           1                                                            // set by target:MCU_STM32
+#define MBED_CONF_TARGET_INTERNAL_FLASH_UNIFORM_SECTORS                   1                                                            // set by target:Target
+#define MBED_CONF_TARGET_LPTICKER_LPTIM                                   1                                                            // set by target:MCU_STM32H7
+#define MBED_CONF_TARGET_LPTICKER_LPTIM_CLOCK                             1                                                            // set by target:MCU_STM32
+#define MBED_CONF_TARGET_LPUART_CLOCK_SOURCE                              USE_LPUART_CLK_LSE|USE_LPUART_CLK_PCLK1|USE_LPUART_CLK_PCLK3 // set by target:MCU_STM32
+#define MBED_CONF_TARGET_LSE_AVAILABLE                                    1                                                            // set by target:MCU_STM32
+#define MBED_CONF_TARGET_LSE_DRIVE_LOAD_LEVEL                             RCC_LSEDRIVE_LOW                                             // set by target:MCU_STM32H7
+#define MBED_CONF_TARGET_MPU_ROM_END                                      0x0fffffff                                                   // set by target:Target
+#define MBED_CONF_TARGET_NETWORK_DEFAULT_INTERFACE_TYPE                   ETHERNET                                                     // set by target:NUCLEO_H743ZI2
+#define MBED_CONF_TARGET_RTC_CLOCK_SOURCE                                 USE_RTC_CLK_LSE_OR_LSI                                       // set by target:MCU_STM32
+#define MBED_CONF_TARGET_SYSTEM_POWER_SUPPLY                              PWR_LDO_SUPPLY                                               // set by target:MCU_STM32H743xI
+#define MBED_CONF_TARGET_TICKLESS_FROM_US_TICKER                          0                                                            // set by target:Target
+#define MBED_CONF_TARGET_XIP_ENABLE                                       0                                                            // set by target:Target
+#define MBED_CRC_TABLE_SIZE                                               16                                                           // set by library:drivers
+#define MBED_STACK_DUMP_ENABLED                                           0                                                            // set by library:platform
+#define MBED_TRACE_COLOR_THEME                                            0                                                            // set by library:mbed-trace
+#define MBED_TRACE_MAX_LEVEL                                              TRACE_LEVEL_INFO                                             // set by application[*]
+#define MEM_ALLOC                                                         malloc                                                       // set by library:mbed-trace
+#define MEM_FREE                                                          free                                                         // set by library:mbed-trace
+#define STM32_D11_SPI_ETHERNET_PIN                                        PB_5                                                         // set by target:NUCLEO_H743ZI2
+// Macros
+#define _RTE_                                                                                                                          // defined by library:rtos
+
+#endif
